dd4e1e716c443bad335a9fbd2ae5e1b804ba620a
igudy <goodnessigunma1@gmail.com>
2023-10-19 20:34:54 +0100

    excersize 4 done

--- 5. Build a Chrome Extension/33. Your first localStorage/index.js
+++ 5. Build a Chrome Extension/33. Your first localStorage/index.js
@@ -1,9 +1,10 @@
-let myLeads = []
-const inputEl = document.getElementById("input-el")
-const inputBtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("ul-el")
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
+const ulEl = document.getElementById("ul-el");
 
 // 1. Save a key-value pair in localStorage
+
 // 2. Refresh the page. Get the value and log it to the console
 // 3. Clear localStorage
 
@@ -13,23 +14,22 @@ const ulEl = document.getElementById("ul-el")
 // localStorage.clear()
 // PS: both key and value need to be strings
 
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    renderLeads()
-})
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
 
 function renderLeads() {
-    let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += `
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
-        `
-    }
-    ulEl.innerHTML = listItems  
+        `;
+  }
+  ulEl.innerHTML = listItems;
 }
